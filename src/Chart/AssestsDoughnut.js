72802a8a5b336327f1b716c96dda8b443c5ff5fe
Tasnim Pricila <tasnimtanzim55@gmail.com>
2023-09-14 22:58:09 +0600

    assets chart done

---
+++ src/Chart/AssestsDoughnut.js
@@ -0,0 +1,42 @@
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+
+Chart.register(CategoryScale);
+const AssestsDoughnut = () => {
+  const assetsDatapoints = [55, 15, 10, 5]
+
+  const assetsData = {
+    // labels: ["BTC", "ETH", "ADA", "Others"],
+    datasets: [
+      {
+        // label: labels,
+        data: assetsDatapoints,
+        backgroundColor: ["#3380FF", "#B1D3FF", "#00C4DF", "#155AB6"],
+        
+      },
+    ],
+  };
+  return (
+    <div>
+      <Doughnut
+        data={assetsData}
+        options={{
+          responsive: true,
+          plugins: {
+            datalabels: {
+                display: true,
+                color: "red"
+            }
+          },
+          animation: {
+            duration: 4000,
+          },
+          
+        }}
+      />
+    </div>
+  );
+};
+
+export default AssestsDoughnut;
--- src/UI/Assests.js
+++ src/UI/Assests.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React from "react";
+import AssestsDoughnut from "../Chart/AssestsDoughnut";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquare } from "@fortawesome/free-regular-svg-icons";
 
 const Assests = () => {
-    return (
-        <div className='basis-1/4 bg-white h-48 rounded-[15px] px-[29px] py-[21px]'>
-            
+  return (
+    <div className="basis-1/4 bg-white rounded-[15px] px-[29px] py-[21px] border-fuchsia-500">
+      <p className="font-semibold text-[20px]">Assets</p>
+      <AssestsDoughnut />
+      <div className="grid grid-cols-4 justify-items-center mt-4">
+        <div className="bg-[#0060ff1a] px-[7px] py-[2px] rounded-[5px] inline  text-[10px]">
+          <FontAwesomeIcon
+            icon={faSquare}
+            className="bg-[#3380FF] text-[#3380FF]"
+          />
+          <span className="text-[#2F80ED] pl-1">BTC</span>
         </div>
-    );
+        <div className="bg-[#0060ff1a] px-[7px] py-[2px] rounded-[5px] inline  text-[10px]">
+          <FontAwesomeIcon
+            icon={faSquare}
+            className="bg-[#B1D3FF] text-[#B1D3FF]"
+          />
+          <span className="text-[#2F80ED] pl-1">ETH</span>
+        </div>
+        <div className="bg-[#0060ff1a] px-[7px] py-[2px] rounded-[5px] inline  text-[10px]">
+          <FontAwesomeIcon
+            icon={faSquare}
+            className="bg-[#00C4DF] text-[#00C4DF]"
+          />
+          <span className="text-[#2F80ED] pl-1">ADA</span>
+        </div>
+        <div className="bg-[#0060ff1a] px-[7px] py-[2px] rounded-[5px] inline  text-[10px]">
+          <FontAwesomeIcon
+            icon={faSquare}
+            className="bg-[#155AB6] text-[#155AB6]"
+          />
+          <span className="text-[#2F80ED] pl-1">Others</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Assests;
